dea6f45905d3961f105c550b8bc2956caf24e490
Leonard Rapp <leonardrapp@gmx.de>
2016-12-14 22:54:24 +0100

    send singe color command on seek bar change

--- app/src/main/java/de/lrapp/ledpp/MainActivity.java
+++ app/src/main/java/de/lrapp/ledpp/MainActivity.java
@@ -8,26 +8,47 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener,
+        SeekBar.OnSeekBarChangeListener  {
 
     final int MY_PERMISSIONS_REQUEST_BLUETOOTH = 42;
 
+    final String TAG = "LEDpp - MainActivity";
+
     Spinner btDevsSpinner;
+    SeekBar redSeekBar;
+    SeekBar greenSeekBar;
+    SeekBar blueSeekBar;
     BtComm btComm;
     ArrayAdapter<String> btDevsSpinnerAdapter;
+
+    private byte[] color = {0, 0, 0};
+
+    private final char cmdStart = 'S';
+    private final  char cmdEnd = 'E';
+    private final char cmdSingleColor = 'a';
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // initialize views, set change listeners
         btDevsSpinner = (Spinner) findViewById(R.id.bt_devs_spinner);
+        redSeekBar = (SeekBar) findViewById(R.id.seekBar_red);
+        greenSeekBar = (SeekBar) findViewById(R.id.seekBar_green);
+        blueSeekBar = (SeekBar) findViewById(R.id.seekBar_blue);
+        redSeekBar.setOnSeekBarChangeListener(this);
+        greenSeekBar.setOnSeekBarChangeListener(this);
+        blueSeekBar.setOnSeekBarChangeListener(this);
         btDevsSpinner.setOnItemSelectedListener(this);
         btComm = new BtComm();
 
@@ -104,6 +125,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         // connect to selected device
         if (btComm.connect(pos)) {
             makeToast("Connected to: " + parent.getItemAtPosition(pos).toString());
+            setSingleColor();
         } else {
             makeToast("Connection failed!");
         }
@@ -126,5 +148,62 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         int duration = Toast.LENGTH_SHORT;
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
+    }
+
+    /**
+     * sends single color command
+     */
+    private void setSingleColor() {
+        Log.i(TAG, "singleColor");
+        byte[] data = new byte[6];
+        data[0] = cmdStart;
+        data[1] = cmdSingleColor;
+        data[2] = color[0];
+        data[3] = color[1];
+        data[4] = color[2];
+        data[5] = cmdEnd;
+        btComm.send(data);
+    }
+
+    /**
+     * Callback method when seek bar progress has changed, reads color values from seek bar
+     * @param seekBar The SeekBar whose progress has changed
+     * @param progress The current progress level
+     * @param fromUser True if the progress change was initiated by the user
+     */
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        int id = seekBar.getId();
+        Log.i(TAG, "seek bar progress changed");
+        switch (id) {
+            case R.id.seekBar_red:
+                color[0] = (byte) progress;
+                break;
+            case R.id.seekBar_green:
+                color[1] = (byte) progress;
+                break;
+            case R.id.seekBar_blue:
+                color[2] = (byte) progress;
+                break;
+        }
+        setSingleColor();
+    }
+
+    /**
+     * Unused seek bar callback method
+     * @param seekBar The SeekBar in which the touch gesture began
+     */
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    /**
+     * Unused seek bar callback method
+     * @param seekBar The SeekBar in which the touch gesture began
+     */
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
     }
 }
